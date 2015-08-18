e32a520a1c860669b3bad67d4b5e0bcad410003d
hanjianqiao <hanjianqiao@gmail.com>
2015-08-18 17:48:11 +0800

    Affinity propagation is amost OK

--- algorithm/AP.py
+++ algorithm/AP.py
@@ -1,53 +1,92 @@
-print(__doc__)
 
+# coding: utf-8
+
+# In[144]:
+
+array_length = 100
+
+
+# In[145]:
 
 from sklearn.cluster import AffinityPropagation
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 
-##############################################################################
-# Generate sample data
-centers = [[1, 1], [-1, -1], [1, -1]]
-X, labels_true = make_blobs(n_samples=300, centers=centers, cluster_std=0.5,
-                            random_state=0)
 
-##############################################################################
-# Compute Affinity Propagation
-af = AffinityPropagation(preference=-50).fit(X)
-cluster_centers_indices = af.cluster_centers_indices_
-labels = af.labels_
+# In[146]:
 
-n_clusters_ = len(cluster_centers_indices)
+import  numpy as np
 
-print('Estimated number of clusters: %d' % n_clusters_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f"
-      % metrics.adjusted_rand_score(labels_true, labels))
-print("Adjusted Mutual Information: %0.3f"
-      % metrics.adjusted_mutual_info_score(labels_true, labels))
-print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(X, labels, metric='sqeuclidean'))
 
-##############################################################################
-# Plot result
+# In[147]:
+
+array = np.ndarray(shape=(20,array_length), dtype=float, order='F')
+
+
+# In[148]:
+
+import math
+import random
+
+
+# In[149]:
+
+for j in range(0, 5):
+    for i in range(0, array_length):
+        array[j][i] = math.sin(i/10.0)*i+random.random()
+
+
+# In[150]:
+
+for j in range(5, 10):
+    for i in range(0, array_length):
+        array[j][i] = math.cos(i/10.0)*i+random.random()
+
+
+# In[151]:
+
+for j in range(10, 15):
+    for i in range(0, array_length):
+        array[j][i] = math.sin(i/10.0)*i+random.random()
+
+
+# In[152]:
+
+for j in range(15, 20):
+    for i in range(0, array_length):
+        array[j][i] = i*math.cos(i/10.0)*i+random.random()
+
+
+# In[153]:
+
+af = AffinityPropagation(preference=-50).fit(array)
+
+
+# In[154]:
+
+import numpy as np
 import matplotlib.pyplot as plt
-from itertools import cycle
 
 plt.close('all')
 plt.figure(1)
 plt.clf()
+x = np.arange(0, array_length, 1);
+for i in range(0 , 20):
+    y = array[i]
+    plt.plot(x, y, '#eeefff')
 
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    class_members = labels == k
-    cluster_center = X[cluster_centers_indices[k]]
-    plt.plot(X[class_members, 0], X[class_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-    for x in X[class_members]:
-        plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
-
-plt.title('Estimated number of clusters: %d' % n_clusters_)
+for i in af.cluster_centers_indices_:
+    y = array[i]
+    plt.plot(x, y, 'r')
 plt.show()
+
+
+# In[67]:
+
+
+
+
+# In[ ]:
+
+
+
