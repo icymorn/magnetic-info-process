1e69e9bf2d2496044138312b53533627f0c02e04
icymorn <moevery@gmail.com>
2015-08-28 10:53:34 +0800

    modify gitignore

--- data/DataLoader.py
+++ data/DataLoader.py
@@ -1,22 +1,28 @@
 import pickle
 import json
 
+dataset = {}
+
 class DataLoader:
     def __init__(self):
         pass
 
     def read(self, filename):
-        if filename.endswith('.json'):
-            data = json.load(open(filename, 'rb'))
-        else:
-            data = pickle.load(open(filename, 'rb'))
+        data = dataset.get(filename)
+        if data is None:
+            if filename.endswith('.json'):
+                data = json.load(open(filename, 'rb'))
+            else:
+                data = pickle.load(open(filename, 'rb'))
+            dataset[filename] = data
         return data
 
 
 def main():
     data = DataLoader()
     # print config.data
-    print data.read("out0.json")
+    print data.read("a0.data")
+    print data.read("a0.data")
 
 if __name__ == '__main__':
     main()
